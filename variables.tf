4934e225434a4372ab25aa88b4b04d7a2c296c1d
roshanthomas9495 <157958499+roshanthomas9495@users.noreply.github.com>
2024-05-29 18:51:58 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region"
+  type        = string
+}
+
+variable "zone" {
+  description = "The GCP zone"
+  type        = string
+}
+
+variable "audience" {
+  description = "The OIDC audience"
+  type        = string
+}
